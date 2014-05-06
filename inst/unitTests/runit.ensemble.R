82103d2c64b1373882d35ac3b24fdd5fcff7c1f0
Brian Lee Yung Rowe <rowe@nurometic.com>
2014-05-06 10:26:56 -0400

    Added Travis CI support

--- inst/unitTests/runit.ensemble.R
+++ inst/unitTests/runit.ensemble.R
@@ -3,8 +3,8 @@ test.max_eigen <- function()
 {
   en <- Ensemble(50, WishartModel(50, 200))
   mx <- max_eigen(en)
-  theo <- qmp(1, svr=4, var=1)
-  cat("\ntheoretical cutoff:",theo,"\n")
-  print(mx)
+  #theo <- qmp(1, svr=4, var=1)
+  #cat("\ntheoretical cutoff:",theo,"\n")
+  #print(mx)
   #checkEquals(rep(theo, length(mx)), mx, tolerance=0.1)
 }
